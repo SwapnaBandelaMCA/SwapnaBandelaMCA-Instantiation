16f3c5ed1ac4ba30cbd5d6473dfbdbd618afcd6e
Swapna Bandela <bandela.swapna@gmail.com>
2023-03-06 17:59:09 +0000

    Week2Day1 IntroToOOP lab done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Lab coach = new Lab();
+        return coach;
     }
 }
